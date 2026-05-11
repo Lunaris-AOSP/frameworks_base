fcb3333a1e4c8268e218d944be8d00b6c0489971
TwistedVision518 <195054967+TwistedVision518@users.noreply.github.com>
2026-05-11 05:11:08 +0000

    SystemUI: Prevent UDFPS screen flash during sleep transition

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -2175,9 +2175,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, CoreSt
                 cb.onFinishedGoingToSleep(arg1);
             }
         }
-        final int userId = mSelectedUserInteractor.getSelectedUserId();
-        final boolean canFpUnlock = isUnlockWithFingerprintPossible(userId);
-        if (canFpUnlock && mDozeParameters.get().getAlwaysOn()) {
+        if (isUdfpsSupported() && mDozeParameters.get().getAlwaysOn()) {
             mGoingToSleep = false;
         }
         updateFingerprintListeningState(BIOMETRIC_ACTION_UPDATE);
